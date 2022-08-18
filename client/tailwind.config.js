f61888998d9e42298de9f086896ae56543fc9d41
rastaxarm <xarmzon@gmail.com>
2022-08-18 13:54:18 +0000

    update to client

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["poppins", "sans-serif"],
+      },
+      colors: {
+        primary: {
+          DEFAULT: "rgb(60,68,148)",
+          100: "rgb(102,59,125)",
+        },
+        secondary: {
+          DEFAULT: "rgb(220,43,51)",
+          100: "rgb(251,131,139)",
+          200: "rgb(221,82,90)",
+          300: "rgb(183,52,76)",
+        },
+        deep: {
+          DEFAULT: "#120508",
+          100: "#371017",
+          200: "#371017",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
