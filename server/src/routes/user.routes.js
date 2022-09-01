91f2cc47771d530174f26d5f33f86bb6267f6b6b
EAZYLINK <eazy4life2017@gmail.com>
2022-09-01 10:23:42 +0100

    user routes modified

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const { getUserById, createUser, getAllUsers, updateUser, deleteUser } = require("../Controllers/users.controller");
+const { adminRequired, userRequired } = require("../middleware/auth.middleware");
 const userRouter = express.Router();
 
 
 userRouter.get("/", getUserById);
 userRouter.post("/", createUser);
-userRouter.get("/admin", getAllUsers);
-userRouter.put("/", updateUser);
-userRouter.delete("/", deleteUser)
+userRouter.get("/admin", adminRequired, getAllUsers);
+userRouter.put("/", userRequired, updateUser);
+userRouter.delete("/", userRequired, deleteUser)
 
 
 
