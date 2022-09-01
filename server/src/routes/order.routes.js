dbca9b47673ab340acdd9ef9698600b6d3b9ff6f
EAZYLINK <eazy4life2017@gmail.com>
2022-09-01 10:20:18 +0100

    order routes modified

--- server/src/routes/order.routes.js
+++ server/src/routes/order.routes.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const { createOrder, getAllOrder, updateOrder, updateOrderStatus, getOrderById, deleteOrder } = require("../Controllers/order.controller");
+const { adminRequired, userRequired } = require("../middleware/auth.middleware");
 const orderRouter = express.Router();
 
-orderRouter.post("/", createOrder);
-orderRouter.get("/admin", getAllOrder);
-orderRouter.get("/", getOrderById)
-orderRouter.delete("/", deleteOrder)
-orderRouter.put("/", updateOrder),
-orderRouter.put("/status", updateOrderStatus);
-
-
+orderRouter.post("/", userRequired, createOrder);
+orderRouter.get("/admin", userRequired, adminRequired, getAllOrder);
+orderRouter.get("/", userRequired, getOrderById)
+orderRouter.delete("/", userRequired, deleteOrder)
+orderRouter.put("/", userRequired, updateOrder),
+orderRouter.put("/status", userRequired, adminRequired, updateOrderStatus);
 
 
 module.exports ={
