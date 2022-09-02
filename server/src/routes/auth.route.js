fd1bc4e864e2ff6626178239ec00d5a0bd4ae50d
Ezekiel Nwuguru <eazy4life2017@gmail.com>
2022-09-02 01:12:22 +0100

    middleware added to logout and refreshToken endpoint

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const authRouter = express.Router();
 const {register, login, logout, refreshToken} = require("../Controllers/auth.controller")
+const { userRequired } = require("../middleware/auth.middleware");
 
 authRouter.post("/register", register);
 authRouter.post("/login", login);
-authRouter.post("/logout", logout);
-authRouter.post("/refreshToken", refreshToken);
+authRouter.post("/logout", userRequired, logout);
+authRouter.post("/refreshToken", userRequired, refreshToken);
 
 
 
